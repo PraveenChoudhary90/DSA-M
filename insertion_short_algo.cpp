b03c32932b61a0ef370e6e61abe228860db470f8
praveen choudhary <pc852180@gmail.com>
2025-01-24 14:22:42 +0530

    Insertin sort algorithim

---
+++ insertion_short_algo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void insrt(int arr[],int s){
+    for(int  i = 0;i<s;i++){
+        int mn = i;
+        while(mn >=0 && arr[mn]<arr[mn-1]){
+            int temp;
+            temp = arr[mn-1];
+            arr[mn-1]=arr[mn];
+            arr[mn]=temp;
+            mn--;
+
+        }
+    }
+    cout << "after appling insertin short" << "\n";
+    for(int  i = 0;i<s;i++){
+        cout << arr[i] << "\t";
+    }
+}
+int main(){
+    int arr[]={10,5,50,8,20};
+    int s = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i<s;i++){
+        cout << arr[i] << "\t";
+    }
+    insrt(arr , s);
+}
