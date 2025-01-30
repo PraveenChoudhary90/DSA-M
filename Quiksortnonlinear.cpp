28878982e668abea54d877539067ba7f631c29bf
praveen choudhary <pc852180@gmail.com>
2025-01-30 18:17:25 +0530

    quik sort

--- Quiksortnonlinear.cpp
+++ Quiksortnonlinear.cpp
@@ -1,2 +1,49 @@
 #include <iostream>
 using namespace std;
+int part (int arr[],int low,int high){
+
+    int pivot =arr[low];
+    int i = low+1;
+    int j = high;
+    do{
+        while(arr[i]<pivot){
+            i++;
+        }
+        while(arr[j]>pivot){
+            j--;
+        }
+        if(i<j){
+            int temp;
+            temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    while(i<j);
+    int temp;
+    temp=arr[low];
+    arr[low]=arr[j];
+    arr[j]=temp;
+    return j;
+}
+void qksrt(int arr[],int low,int high){
+    int pv;
+    if(low<high){
+        pv=part(arr,low,high);
+        qksrt(arr,0,pv-1);
+        qksrt(arr,pv+1,high);
+    }
+}
+int main(){
+    int arr[]={5,9,5,6,2};
+    int s = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"\n";
+    qksrt(arr,0,s-1);
+     for(int i=0;i<s;i++){
+        cout<<arr[i]<<"\t";
+    }
+
+}
