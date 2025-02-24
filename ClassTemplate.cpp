d6cc376f613cb656602ac08b7389a37518142a8b
praveen choudhary <pc852180@gmail.com>
2025-02-24 13:27:38 +0530

    class template

---
+++ ClassTemplate.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+template<typename x1 , typename x2>
+class bhopal{
+    public:void sum(x1 t1, x2 t2){
+        cout << t1+t2 << "\n";
+    }
+};
+int main(){
+    bhopal <int, double>b;
+    b.sum(3,4.6);
+}
