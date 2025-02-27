da3a8e3d6b24021ebe3c13828f8ac9f403d15fae
praveen choudhary <pc852180@gmail.com>
2025-02-27 13:33:13 +0530

    Array template

--- ArrayTemplate/SecondLast.cpp
+++ ArrayTemplate/SecondLast.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(){
     array<int , 6>arr{10,32,4,3,5,7};
     sort(arr.begin(), arr.end());
-    // cout << *(arr.end()-2) <<"\n";
+    // cout << *(arr.end()-2) <<"\n"; 1,4,5,6,7,8[end]-2
     cout << arr[arr.size()-2] << "\n";
 }
