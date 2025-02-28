96c63169a0f245d1f652e6afb7bb901af76c3b41
praveen choudhary <pc852180@gmail.com>
2025-02-28 13:50:30 +0530

    vector size and capacity

--- Vector/Simplevector.cpp
+++ Vector/Simplevector.cpp
@@ -3,6 +3,15 @@ using namespace std;
 #include <vector>
 #include <algorithm>
 int main(){
+    // // vector<int>v{12,3,4,55};
+    // cout << "size=" << v.size() << "\n";
+    // cout << "capacity=" << v.capacity() << "\n";
     vector<int>v;
+    v.push_back(23);
+    v.push_back(2);
+    v.push_back(29);
+    v.push_back(90);
+    v.push_back(100);
     cout << "size=" <<v.size();
+    cout << "capacity=" <<v.capacity();
 }
