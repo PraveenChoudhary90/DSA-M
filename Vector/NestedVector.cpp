e3d9900bf271aa706b782efef2bed190919386ac
praveen choudhary <pc852180@gmail.com>
2025-03-04 13:23:09 +0530

    Nestetd vector ink list

--- Vector/NestedVector.cpp
+++ Vector/NestedVector.cpp
@@ -16,14 +16,14 @@ int main(){
             for(int j = 0;j<col;j++){
                 cout << "Enter the values" << "\n";
                 cin >> val;
-                v2.push_back(val);
+                v2.push_back(val);//v2 me val ki puri value leta hai
             }
-            v1.push_back(v2);
+            v1.push_back(v2);//v1 me v2 ki puri value le raha hai
         }
     }
     cout << "Store the values in 2d vector" << "\n";
-    for(int i=0;i<v1.size();i++){
-        for(int j=0;j<v1[i].size();j++){
+    for(int i=0;i<v1.size();i++){//row 
+        for(int j=0;j<v1[i].size();j++){//col
             cout << v1[i][j] << "\t";
         }
         cout << "\n";
