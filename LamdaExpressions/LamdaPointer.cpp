7cc4ec8d6b58b510a8144bcebc44d8f60e2b874b
praveen choudhary <pc852180@gmail.com>
2025-03-05 13:46:50 +0530

    Lamda expression

--- LamdaExpressions/LamdaPointer.cpp
+++ LamdaExpressions/LamdaPointer.cpp
@@ -7,5 +7,7 @@ bool cybrom(int a , int b){
 }
 int main(){
     vector<int>v{1,2,3,4,5,5};
-    cout << [](int a){return a+2;}(5);
+    cout << [](int a){return a+2;}(5)<<"\n";
+    auto p = [](int a, int b){return a+b;};
+    cout<<p(45,9) << "\n";
 }
