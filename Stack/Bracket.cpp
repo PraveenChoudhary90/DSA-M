951eaff6ddc337afeb15fd0b9d887d23adb38142
praveen choudhary <pc852180@gmail.com>
2025-03-07 13:30:19 +0530

    stack making and header file

---
+++ Stack/Bracket.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+bool valid (string s){
+
+
+}
+int main(){
+    string s;
+    cout << "Enter the Combination of brackets " << "\n";
+    cin>>s;
+    if(valid(s)){
+        cout << "valid" << "\n";
+    }
+    else{
+        cout << "Invalid" << "\n";
+    }
+}
