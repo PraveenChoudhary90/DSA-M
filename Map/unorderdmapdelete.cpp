eb6c8e3f0c4ffb557f45b65d40bbab99256b1a95
praveen choudhary <pc852180@gmail.com>
2025-03-22 19:29:01 +0530

    Unorderdmap delete

---
+++ Map/unorderdmapdelete.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    unordered_map<int, string>up;
+    up.insert({104, "praveen"});
+    up.insert({704, "anu"});
+    up.insert({10, "chotu"});
+    for(auto p:up){
+        cout << p.first << ":" << p.second << "\n";
+    }
+    auto t = up.erase(10);
+    for(auto p:up){
+        cout << p.first << ":" << p.second << "\n";
+    }
+}
