115c79ec06f9a40ad6714b0a94350b8ee8a5d3ab
praveen choudhary <pc852180@gmail.com>
2025-03-28 17:36:48 +0530

    Daynamic tree entery

--- BanairyTree/DaynamicBtree.cpp
+++ BanairyTree/DaynamicBtree.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
 class node{
-    
-}
+    public:int data;
+    node *left;
+    node *right;
+    node (int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    };
+
+};
