06241c2669976a0d5add77f3cae58ad36414d17d
praveen choudhary <pc852180@gmail.com>
2025-03-28 22:20:57 +0530

    daynimic tree entery

--- BanairyTree/DaynamicBtree.cpp
+++ BanairyTree/DaynamicBtree.cpp
@@ -10,5 +10,59 @@ class node{
         left = NULL;
         right = NULL;
     };
+    node *create(node *root)
+    {
+        int d;
+        cout << "enter values" << "\n";
+        cin >> d;
+        root = new node(d);
+        if(d==-1){
+            return NULL;
+        }
+        cout << "Enter Value fr left" << d << "\n";
+        root->left = create(root->left);
+        cout << "Enter value for right" << "d" << "\n";
+        root->right = create(root->right);
+        return root;
+    }
+    void preorder (node *n){
+        if(n==NULL){
+            return ;
+        }
+        cout << n->data << "\t";
+        preorder(n->left);
+        preorder(n->right);
+    }
+    void inorder(node *n){
+        if(n==NULL)
+        {
+            return;
+        }
+        inorder(n->left);
+        cout << n->data << "\t";
+        inorder(n->right);
+    }
+      void postorder(node *n){
+        if(n==NULL)
+        {
+            return;
+        }
+        postorder(n->left);
+        postorder(n->right);
+        cout << n->data << "\t";
+
+    }
+   
 
 };
+ int main(){
+    node *root = NULL;
+    root=create(root);
+    cout << "\nPreorder\n";
+    preorder(root);
+    cout << "\nInorder\n";
+    inorder(root);
+    cout << "\nPOSTOrder\n";
+    postorder(root);
+        
+}
